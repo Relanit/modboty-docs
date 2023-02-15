674bc457f0a6822a0571cf6a2ed13cc36624cd0b
Relanit <stulovmaxim3@gmail.com>
2023-02-15 19:26:58 +0300

    refactor

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'ModBoty',
   tagline: 'Упростите работу модераторов',
-  url: 'https://relanit.github.io/',
-  baseUrl: '/modboty-docs/',
+  url: 'https://docs.modbot.xyz/',
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/MODS.ico',
