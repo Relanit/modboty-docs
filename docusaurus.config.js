e6b4dbe536693d46531080f48cad4d268bc19a47
Relanit <stulovmaxim3@gmail.com>
2023-02-15 20:20:01 +0300

    refactoring

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,8 +16,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'Relanit', // Usually your GitHub org/user name.
-  projectName: 'modboty-docs', // Usually your repo name.
+  // organizationName: 'Relanit', // Usually your GitHub org/user name.
+  // projectName: 'modboty-docs', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
